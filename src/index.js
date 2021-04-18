3ef0306d0b92e30f461f4ce786240839518acc34
Marcelo Ragnelli Frigerio <mrfrigerio@yahoo.com.br>
2021-04-18 12:41:55 -0300

    Implementing features

--- src/index.js
+++ src/index.js
@@ -1,33 +1,85 @@
 const express = require('express');
 const cors = require('cors');
-
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers
+
+  if (!username) {
+    return response.status(400).json({error: "Bad request. The username is required on header!"})
+  }
+  const userExists = users.find((user) => user.username === username)
+
+  if (!userExists) {
+    return response.status(400).json({ error: "User not exists!"})
+  }
+
+  request.user = userExists
+  return next()
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const { name, username } = request.body
+  if(users.some((user) => user.username === username)){
+    return response.status(400).json({error: "User already exists!"})
+  }
+
+  const newUser = {
+    id: uuidv4(),
+    name,
+    username,
+    todos: []
+  }
+  users.push(newUser)
+  return response.status(200).json(newUser)
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request
+  return response.json(user.todos)
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+
+  const { title, deadline } = request.body
+  const { user } = request
+  const newTodo = {
+    id: uuidv4(),
+    title,
+    done: false,
+    deadline,
+    created_at: new Date()
+  }
+
+  user.todos.push(newTodo)
+
+  return response.status(201).json(newTodo)
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { title, deadline } = request.body
+  const { id } = request.params
+
+  const { user } = request
+  const todo = user.todos.find((t) => t.id === id)
+
+  if(!todo) {
+    return response.status(400).json({error: "Todo not found!"})
+  }
+
+  Object.assign(todo, {
+    title,
+    deadline,
+  })
+
+  return response.status(200).json(todo)
 });
 
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
